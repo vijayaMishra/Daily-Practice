cc76221f9d2bcb4092dd81a5069048fbb5e5c2f7
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-15 15:55:30 +0530

    converting all fruits to uppercase

---
+++ array functions/convertToUpperCase.js
@@ -0,0 +1,7 @@
+// Input: ["apple", "banana", "cherry"]
+// Output: ["APPLE", "BANANA", "CHERRY"]
+
+let fruits = ["apple", "banana", "cherry"];
+
+let bigFruits = fruits.map(fruit => fruit.toUpperCase());
+console.log(bigFruits);
