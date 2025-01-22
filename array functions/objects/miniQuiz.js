abfa01cdefb7bc92030d514221cdcc918b155164
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-22 19:16:45 +0530

    mini quiz 2

--- array functions/objects/miniQuiz.js
+++ array functions/objects/miniQuiz.js
@@ -1,6 +1,13 @@
-let obj = {
-    JS_teacher: "Ankit",
-    student: "Vijju"
-}
+// let obj = {
+//     JS_teacher: "Ankit",
+//     student: "Vijju"
+// }
 
-console.log(`${obj.name} teaches ${obj.wife} with love.`);
+// console.log(`${obj.name} teaches ${obj.wife} with love.`);
+
+var quiz = {
+    question: "What is JavaScript?",
+    answer: "A programming language."
+}
+console.log(quiz.question);
+console.log(quiz.answer);
