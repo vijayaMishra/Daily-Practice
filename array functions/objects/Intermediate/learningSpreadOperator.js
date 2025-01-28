ea90bf03eb269dca97ce07bf81b3a430068c45a2
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-28 22:17:22 +0530

    Combining two arrays using spread operator

--- array functions/objects/Intermediate/learningSpreadOperator.js
+++ array functions/objects/Intermediate/learningSpreadOperator.js
@@ -3,3 +3,9 @@ var arr1 = [1, 2, 3];
 var arr2 = [ ...arr1 ];
 console.log("arr2", arr2);
 console.log("arr1", arr1);
+
+// Combining arrays
+var arr3 = [4, 5, 6];
+
+const combinedArr = [ ...arr1, ...arr3];
+console.log(combinedArr);
