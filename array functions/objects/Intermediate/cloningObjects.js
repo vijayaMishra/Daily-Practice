a5748c8eb8b2ea598a1a0df60860a56cfc659bb6
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-28 22:01:51 +0530

    Understanding Spread Operator

---
+++ array functions/objects/Intermediate/cloningObjects.js
@@ -0,0 +1,4 @@
+// Explain the difference between shallow copy and deep copy of an object.
+
+// Write code to create a shallow copy of an object using Object.assign.
+
---
+++ array functions/objects/Intermediate/learningSpreadOperator.js
@@ -0,0 +1,5 @@
+var arr1 = [1, 2, 3];
+
+var arr2 = [ ...arr1 ];
+console.log("arr2", arr2);
+console.log("arr1", arr1);
