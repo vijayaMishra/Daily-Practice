f90981a8f27a7402bd4e89e05ad4bbb02e1db459
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-29 23:16:05 +0530

    object destructuring.js

---
+++ array functions/objects/Intermediate/objectDestructuring.js
@@ -0,0 +1,12 @@
+const user = { 
+    name: "Vijaya", 
+    age: 24, 
+    email: "vijaya@charmee.com" 
+};
+
+//Use destructuring to extract the name and email properties into variables.
+
+const {name, email} = user;
+
+console.log(name);
+console.log(email);
