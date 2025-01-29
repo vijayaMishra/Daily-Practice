09b6ddd91b9b9c9cded442497d1bc28beb12f254
vijayaMishra <vijayamishra1709@gmail.com>
2025-01-29 23:30:52 +0530

    description of object destructuring

--- array functions/objects/Intermediate/objectDestructuring.js
+++ array functions/objects/Intermediate/objectDestructuring.js
@@ -11,3 +11,7 @@ const {name, email} = user;
 console.log(name);
 console.log(email);
 console.log(user.age);
+
+//objectDestructuring in JavaScript is used to extract values from objects and assign them to variables in a concise way. It makes your code cleaner and easier to read.
+
+
